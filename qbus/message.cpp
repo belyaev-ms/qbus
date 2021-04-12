81e667dea42a61db4b824f78b440d60a3d558ee3
Mikhail Belyaev <belyaev_ms@mail.ru>
2021-04-12 15:44:27 +0500

    dev(qbus): started to change arhitecture dmux
    1) added a queue with support for arbitrary message size;
    2) plan to change the name from 'dmux' to 'qbux'

---
+++ qbus/message.cpp
@@ -0,0 +1,232 @@
+#include "qbus/message.h"
+#include <string.h>
+#include <boost/interprocess/detail/atomic.hpp>
+
+namespace qbus
+{
+namespace message
+{
+    
+//==============================================================================
+//  base_message
+//==============================================================================
+/**
+ * Constructor
+ * @param ptr the pointer to the raw message
+ */
+base_message::base_message(void *ptr) :
+    m_ptr(reinterpret_cast<uint8_t*>(ptr))
+{
+}
+
+/**
+ * Constructor
+ * @param ptr the pointer to the raw message
+ * @param cpct the capacity of the message
+ */
+base_message::base_message(void *ptr, const size_t cpct) :
+    m_ptr(reinterpret_cast<uint8_t*>(ptr))
+{
+    memset(ptr, 0, HEADER_SIZE);
+    capacity(cpct);
+}
+
+/**
+ * Destructor
+ */
+//virtual
+base_message::~base_message()
+{
+}
+
+/**
+ * Get the tag of the message
+ * @return the tag of the message
+ */
+tag_type base_message::tag() const
+{
+    return *reinterpret_cast<const tag_type*>(m_ptr + TAG_OFFSET);
+}
+
+/**
+ * Set the tag of the message
+ * @param value the tag of the message
+ */
+void base_message::tag(const tag_type value)
+{
+    *reinterpret_cast<tag_type*>(m_ptr + TAG_OFFSET) = value;
+}
+
+/**
+ * Get the flags of the message
+ * @return the flags of the message
+ */
+flags_type base_message::flags() const
+{
+    return *reinterpret_cast<const flags_type*>(m_ptr + FLAGS_OFFSET);
+}
+
+/**
+ * Set the flags of the message
+ * @param flags the flags of the message
+ */
+void base_message::flags(const flags_type flags)
+{
+    *reinterpret_cast<flags_type*>(m_ptr + FLAGS_OFFSET) = flags;
+}
+
+/**
+ * Get the capacity of the message
+ * @return the capacity of the message
+ */
+size_t base_message::capacity() const
+{
+    return *reinterpret_cast<const uint32_t*>(m_ptr + CAPACITY_OFFSET);
+}
+
+/**
+ * Set the capacity of the message
+ * @param value the capacity of the message
+ */
+void base_message::capacity(const size_t value)
+{
+    *reinterpret_cast<uint32_t*>(m_ptr + CAPACITY_OFFSET) = value;
+}
+
+/**
+ * Get the reference counter of the message
+ * @return the reference counter of the message
+ */
+size_t base_message::counter() const
+{
+    return boost::interprocess::ipcdetail::atomic_read32(reinterpret_cast<uint32_t*>(m_ptr + COUNTER_OFFSET));
+}
+
+/**
+ * Increment reference counter of the message
+ * @return the reference counter of the message
+ */
+size_t base_message::inc_counter()
+{
+    return boost::interprocess::ipcdetail::atomic_inc32(reinterpret_cast<uint32_t*>(m_ptr + COUNTER_OFFSET)) + 1;
+}
+
+/**
+ * Decrement reference counter of the message
+ * @return the reference counter of the message
+ */
+size_t base_message::dec_counter()
+{
+    return boost::interprocess::ipcdetail::atomic_dec32(reinterpret_cast<uint32_t*>(m_ptr + COUNTER_OFFSET)) - 1;
+}
+
+/**
+ * Get the pointer to data of the message
+ * @return the pointer to data of the message
+ */
+void *base_message::data()
+{
+    return m_ptr + DATA_OFFSET;
+}
+
+/**
+ * Get the pointer to data of the message
+ * @return the pointer to data of the message
+ */
+const void *base_message::data() const
+{
+    return m_ptr + DATA_OFFSET;
+}
+
+/**
+ * Pack the data to the message
+ * @param source the pointer to the source of data
+ * @param size the size of the data
+ * @return the size of the packed data
+ */
+size_t base_message::pack(const void *source, const size_t size)
+{
+    const uint8_t *ptr = reinterpret_cast<const uint8_t*>(source);
+    const size_t cpct = capacity();
+    if (size <= cpct)
+    {
+        memcpy(data(), ptr, size);
+        flags(FLG_HEAD | FLG_TAIL);
+        return size;
+    }
+    else
+    {
+        size_t rest_size = 0;
+        memcpy(data(), ptr, cpct);
+        flags(FLG_HEAD);
+        if (m_pmessage)
+        {
+            rest_size = m_pmessage->pack(ptr + cpct, size - cpct);
+            m_pmessage->flags(m_pmessage->flags() & ~FLG_HEAD);
+        }
+        return cpct + rest_size;
+    }
+}
+
+/**
+ * Unpack the data from the message
+ * @param dest the pointer to the destination of data
+ * @return size the size of the unpacked data
+ */
+size_t base_message::unpack(void *dest) const
+{
+    uint8_t *ptr = reinterpret_cast<uint8_t*>(dest);
+    const size_t cpct = capacity();
+    memcpy(ptr, data(), cpct);
+    return cpct + (m_pmessage ? m_pmessage->unpack(ptr + cpct) : 0);
+}
+
+/**
+ * Get the size of attached data
+ * @return the size of attached data
+ */
+size_t base_message::data_size() const
+{
+    return total_capacity();
+}
+
+/**
+ * Get total capacity of all chained messages
+ * @return the total capacity of all chained messages
+ */
+size_t base_message::total_capacity() const
+{
+    return capacity() + (m_pmessage ? m_pmessage->total_capacity() : 0);
+}
+
+/**
+ * Get the size of the message
+ * @return the size of attached data
+ */
+size_t base_message::size() const
+{
+    return static_size(capacity());
+}
+
+/**
+ * Get total size of all chained messages
+ * @return the total size of all chained messages
+ */
+size_t base_message::total_size() const
+{
+    return static_size(capacity()) + (m_pmessage ? m_pmessage->total_size() : 0);
+}
+
+/**
+ * Attach the message
+ * @param pmessage the attached message
+ */
+void base_message::attach(pmessage_type pmessage)
+{
+    
+    m_pmessage = pmessage;
+}
+
+} //namespace message
+
+} //namespace qbus
---
+++ qbus/message.h
@@ -0,0 +1,226 @@
+#ifndef QBUS_MESSAGE_H
+#define QBUS_MESSAGE_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <boost/shared_ptr.hpp>
+#include <boost/smart_ptr/make_shared_object.hpp>
+
+namespace qbus
+{
+    
+namespace message
+{
+
+enum
+{
+    FLG_HEAD = 1,
+    FLG_TAIL = 2
+};
+
+typedef uint32_t tag_type;
+typedef uint32_t flags_type;
+
+/**
+ * The message
+ */
+class base_message
+{
+public:
+    typedef boost::shared_ptr<base_message> pmessage_type;
+    
+    virtual ~base_message();
+    flags_type flags() const; ///< get the flags of the message
+    tag_type tag() const; ///< get the tag of the message
+    void tag(const tag_type value); ///< set the tag of the message
+    size_t counter() const; ///< get the reference counter of the message
+    size_t inc_counter(); ///< increment the reference counter of the message
+    size_t dec_counter(); ///< decrement the reference counter of the message
+    size_t pack(const void *source, const size_t size); ///< pack the data to the message
+    size_t unpack(void *dest) const; ///< unpack the data from the message
+    size_t data_size() const; ///< get the size of attached data
+    size_t size() const; ///< get the size of the message
+    size_t total_size() const; ///< get total size of all chained messages
+    void attach(pmessage_type pmessage); ///< attach the message
+    static size_t static_size(const size_t cpct)
+    {
+        return HEADER_SIZE + cpct;
+    }
+    static size_t static_capacity(const size_t size)
+    {
+        return size > HEADER_SIZE ? size - HEADER_SIZE : 0;
+    }
+protected:
+    base_message(void *ptr);
+    base_message(void *ptr, const size_t cpct);
+private:
+    enum
+    {
+        FLAGS_OFFSET    = 0,
+        FLAGS_SIZE      = sizeof(flags_type),
+        CAPACITY_OFFSET = FLAGS_OFFSET + FLAGS_SIZE,
+        CAPACITY_SIZE   = sizeof(uint32_t),
+        TAG_OFFSET      = CAPACITY_OFFSET + CAPACITY_SIZE,
+        TAG_SIZE        = sizeof(tag_type),
+        COUNTER_OFFSET  = TAG_OFFSET + TAG_SIZE,
+        COUNTER_SIZE    = sizeof(uint32_t),
+        DATA_OFFSET     = COUNTER_OFFSET + COUNTER_SIZE,
+        HEADER_SIZE     = DATA_OFFSET
+    };
+
+    void flags(const flags_type flags); ///< set the flags of the message
+    size_t capacity() const; ///< get the capacity of the message
+    size_t total_capacity() const; ///< get total capacity of all chained messages
+    void capacity(const size_t value); ///< set the capacity of the message
+    void *data(); ///< get the pointer to data of the message
+    const void *data() const; ///< get the pointer to data of the message
+private:
+    uint8_t *m_ptr; ///< the pointer to the raw message
+    pmessage_type m_pmessage; ///< the next message
+};
+
+typedef base_message message_type;
+typedef base_message::pmessage_type pmessage_type;
+
+/**
+ * The message of a queue
+ */
+template <typename Queue>
+class message : public base_message
+{
+public:
+    friend Queue;
+    typedef Queue queue_type;
+    message(void *ptr);
+    message(void *ptr, const size_t cpct);
+private:
+    typedef typename queue_type::region_type region_type;
+    typedef typename queue_type::message_desc_type message_desc_type;
+    static message_desc_type static_make_message(queue_type& queue, const void *data, const size_t size);
+    static message_desc_type static_get_message(const queue_type& queue);
+};
+
+//==============================================================================
+//  message
+//==============================================================================
+/**
+ * Make a message in the queue
+ * @param queue the queue
+ * @param data the pointer to data
+ * @param size the size of data
+ * @return the message description
+ */
+template <typename Queue>
+//static 
+typename message<Queue>::message_desc_type 
+    message<Queue>::static_make_message(queue_type& queue, const void *data, const size_t size)
+{
+    pmessage_type pmessage;
+    pmessage_type pprev_message;
+    pmessage_type plast_message;
+    region_type region;
+    region_type *pprev_region = NULL;
+    size_t rest = size;
+    size_t part = 0;
+    while (rest > 0)
+    {
+        do
+        {
+            region = queue.get_free_region(pprev_region);
+            pprev_region = &region;
+            if (0 == region.second)
+            {
+                return std::make_pair(pmessage_type(), 0);
+            }
+        } while (region.second <= HEADER_SIZE);
+        part = std::min(rest, static_capacity(region.second));
+        plast_message = queue.make_message(queue.data(region.first), part);
+        if (!pmessage)
+        {
+            pmessage = plast_message;
+        }
+        else
+        {
+            pprev_message->attach(plast_message);
+        }
+        pprev_message = plast_message;
+        rest -= part;
+    }
+    if (pmessage->pack(data, size) != size)
+    {
+        abort();
+    }
+    return std::make_pair(pmessage, region.first + plast_message->size());
+}
+
+/**
+ * Get a message in the queue
+ * @param queue the queue
+ * @return the message description
+ */
+template <typename Queue>
+//static 
+typename message<Queue>::message_desc_type 
+    message<Queue>::static_get_message(const queue_type& queue)
+{
+    pmessage_type pmessage;
+    pmessage_type pprev_message;
+    pmessage_type plast_message;
+    region_type region;
+    region_type *pprev_region = NULL;
+    do
+    {
+        do
+        {
+            region = queue.get_busy_region(pprev_region);
+            pprev_region = &region;
+            if (0 == region.second)
+            {
+                return std::make_pair(pmessage_type(), 0);
+            }
+        } while (region.second <= HEADER_SIZE);
+        plast_message = queue.make_message(queue.data(region.first));
+        if (!pmessage)
+        {
+            pmessage = plast_message;
+            assert(pmessage->flags() & FLG_HEAD);
+        }
+        else
+        {
+            pprev_message->attach(plast_message);
+        }
+        pprev_message = plast_message;
+    } while (!(plast_message->flags() & FLG_TAIL));
+    return std::make_pair(pmessage, region.first + plast_message->size());
+}
+
+/**
+ * Constructor
+ * @param ptr the pointer to the raw message
+ */
+template <typename Queue>
+message<Queue>::message(void *ptr) :
+    base_message(ptr)
+{
+}
+
+/**
+ * Constructor
+ * @param ptr the pointer to the raw message
+ * @param cpct the capacity of the message
+ */
+template <typename Queue>
+message<Queue>::message(void *ptr, const size_t cpct) :
+    base_message(ptr, cpct)
+{
+}
+
+} //namespace message
+
+typedef message::pmessage_type pmessage_type;
+
+} //namespace qbus
+
+
+#endif /* MESSAGE_H */
+
