40fd34fb967ba326f7b63cd1d4da8e120f9d33e9
Mikhail Belyaev <belyaev_ms@mail.ru>
2021-05-28 11:54:47 +0500

    dev(pingpong)

--- examples/pingpong.cpp
+++ examples/pingpong.cpp
@@ -26,17 +26,38 @@ const std::vector<char> MESSAGE { 'P', 'I', 'N', 'G', '\0' };
 
 bool qbus_client(const std::string& name)
 {
-    const struct timespec ts = { 10, 0 };
     SDEBUG("qbus::client ");
-    pconnector_type pconnector = connector::make<single_output_connector_type>(name);
-    if (pconnector->open())
+    const struct timespec ts = { 10, 0 };
+    const std::string name_in = name + "_in";
+    const std::string name_out = name + "_out";
+    pconnector_type pconnector_out = connector::make<single_output_connector_type>(name_out);
+    pconnector_type pconnector_in = connector::make<single_input_connector_type>(name_in);
+    if (pconnector_out->open() && pconnector_in->open())
     {
         SDEBUG("open" << std::endl);
         size_t t = time_us();
         for (int i = 0; i < 1024; ++i)
         {
-            pconnector->push(0, &MESSAGE[0], MESSAGE.size(), ts);
+            pconnector_out->push(0, &MESSAGE[0], MESSAGE.size(), ts);
             SDEBUG(":> " << i << "\t" << &MESSAGE[0] << std::endl);
+            pmessage_type pmessage = pconnector_in->get(ts);
+            if (!pmessage)
+            {
+                return false;
+            }
+            const size_t size = pmessage->data_size();
+            if (0 == size)
+            {
+                return false;
+            }
+            if (0 == t)
+            {
+                t = time_us();
+            }
+            std::vector<char> buffer(size);
+            pmessage->unpack(&buffer[0]);
+            pconnector_in->pop(ts);
+            SDEBUG(":< " << i << "\t" << &buffer[0] << std::endl);
         }
         size_t dt = time_us() - t;
         std::cout << "dt = " << dt << std::endl;
@@ -48,34 +69,38 @@ bool qbus_client(const std::string& name)
 
 bool qbus_server(const std::string& name)
 {
-    pconnector_type pconnector = connector::make<single_input_connector_type>(name.c_str());
-    const struct timespec ts = { 600, 0 };
     SDEBUG("qbus::server ");
-    if (pconnector->create(0, 1024))
+    const struct timespec ts = { 10, 0 };
+    const std::string name_in = name + "_in";
+    const std::string name_out = name + "_out";
+    pconnector_type pconnector_out = connector::make<single_output_connector_type>(name_in);
+    pconnector_type pconnector_in = connector::make<single_input_connector_type>(name_out);
+    if (pconnector_in->create(0, 1024) && pconnector_out->create(0, 1024))
     {
         SDEBUG("open" << std::endl);
         size_t t = 0;
         for (int i = 0; i < 1024; ++i)
         {
-            SDEBUG(":> " << i << "\t");
-            pmessage_type pmessage = pconnector->get(ts);
+            pmessage_type pmessage = pconnector_in->get(ts);
             if (!pmessage)
             {
-                break;
+                return false;
             }
             const size_t size = pmessage->data_size();
-            if (size > 0)
+            if (0 == size)
             {
-                if (0 == t)
-                {
-                    t = time_us();
-                }
-                std::vector<char> s(size);
-                pmessage->unpack(&s[0]);
-                SDEBUG(&s[0]);
+                return false;
             }
-            pconnector->pop(ts);
-            SDEBUG(std::endl);
+            if (0 == t)
+            {
+                t = time_us();
+            }
+            std::vector<char> buffer(size);
+            pmessage->unpack(&buffer[0]);
+            pconnector_in->pop(ts);
+            SDEBUG(":< " << i << "\t" << &buffer[0] << std::endl);
+            pconnector_out->push(0, &buffer[0], MESSAGE.size(), ts);
+            SDEBUG(":> " << i << "\t" << &buffer[0] << std::endl);
         }
         size_t dt = time_us() - t;
         std::cout << "dt = " << dt << std::endl;
@@ -85,7 +110,7 @@ bool qbus_server(const std::string& name)
     return false;
 }
 
-bool unix_client(const std::string& name)
+bool unix_socket_client(const std::string& name)
 {
     SDEBUG("unix::client ");
     struct sockaddr_un unix_addr;
@@ -93,7 +118,9 @@ bool unix_client(const std::string& name)
     unix_addr.sun_family = AF_UNIX;
     strcpy(unix_addr.sun_path, name.c_str());
     int sd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (sd != -1 && connect(sd, (struct sockaddr *)&unix_addr, SUN_LEN(&unix_addr)) != -1)
+    int length = MESSAGE.size();
+    if (sd != -1 && connect(sd, (struct sockaddr *)&unix_addr, SUN_LEN(&unix_addr)) != -1 &&
+        setsockopt(sd, SOL_SOCKET, SO_RCVLOWAT, (char *)&length, sizeof(length)) != -1)
     {
         SDEBUG("open" << std::endl);
         size_t t = time_us();
@@ -105,6 +132,13 @@ bool unix_client(const std::string& name)
                 return false;
             }
             SDEBUG(":> " << i << "\t" << &MESSAGE[0] << std::endl);
+            std::vector<char> buffer(MESSAGE.size());
+            if (recv(sd, &buffer[0], buffer.size(), 0) == -1)
+            {
+                close(sd);
+                return false;
+            }
+            SDEBUG(":< " << i << "\t" << &buffer[0] << std::endl);
         }
         size_t dt = time_us() - t;
         std::cout << "dt = " << dt << std::endl;
@@ -115,7 +149,7 @@ bool unix_client(const std::string& name)
     return false;
 }
 
-bool unix_server(const std::string& name)
+bool unix_socket_server(const std::string& name)
 {
     SDEBUG("unix::server ");
     struct sockaddr_un unix_addr;
@@ -135,7 +169,6 @@ bool unix_server(const std::string& name)
             size_t t = time_us();
             for (int i = 0; i < 1024; ++i)
             {
-                SDEBUG(":> " << i << "\t");
                 std::vector<char> buffer(MESSAGE.size());
                 if (recv(csd, &buffer[0], buffer.size(), 0) == -1)
                 {
@@ -143,7 +176,14 @@ bool unix_server(const std::string& name)
                     close(sd);
                     return false;
                 }
-                SDEBUG(&buffer[0] << std::endl);
+                SDEBUG(":< " << i << "\t" << &buffer[0] << std::endl);
+                if (send(csd, &buffer[0], buffer.size(), 0) == -1)
+                {
+                    close(csd);
+                    close(sd);
+                    return false;
+                }
+                SDEBUG(":> " << i << "\t" << &buffer[0] << std::endl);
             }
             size_t dt = time_us() - t;
             std::cout << "dt = " << dt << std::endl;
@@ -159,8 +199,20 @@ bool unix_server(const std::string& name)
 int main(int argc, char** argv)
 {
     std::string name = "qbus_test";
-    bool is_client = false;
-    bool is_qbus = true;
+    enum service_type
+    {
+        SERVICE_SERVER,
+        SERVICE_CLIENT,
+        SERVICE_COUNT
+    };
+    enum transport_type
+    {
+        TRANSPORT_QBUS,
+        TRANSPORT_UNIX_SOCKET,
+        TRANSPORT_COUNT
+    };
+    service_type service = SERVICE_SERVER;
+    transport_type transport = TRANSPORT_QBUS;
     if (argc > 1)
     {
         const char *options = "cun:";
@@ -170,10 +222,10 @@ int main(int argc, char** argv)
             switch (opt)
             {
                 case 'c':
-                    is_client = true;
+                    service = SERVICE_CLIENT;
                     break;
                 case 'u':
-                    is_qbus = false;
+                    transport = TRANSPORT_UNIX_SOCKET;
                     break;
                 case 'n':
                     name = optarg;
@@ -182,28 +234,13 @@ int main(int argc, char** argv)
         }
     }
     
-    if (is_qbus)
+    std::function<bool(const std::string)> handlers[TRANSPORT_COUNT][SERVICE_COUNT] =
     {
-        if (is_client)
-        {
-            qbus_client(name);
-        }
-        else
-        {
-            qbus_server(name);
-        }
-    }
-    else
-    {
-        if (is_client)
-        {
-            unix_client(name);
-        }
-        else
-        {
-            unix_server(name);
-        }
-    }
+        { qbus_server, qbus_client },
+        { unix_socket_server, unix_socket_client }
+    };
+    
+    handlers[transport][service](name);
     
     return 0;
 }
