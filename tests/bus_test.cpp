052df7eb9301d68d3e6a9b7d5ee12e66cd32ade5
Mikhail Belyaev <belyaev_ms@mail.ru>
2022-02-01 20:18:19 +0500

    add(test): added new test case to bus_test

--- tests/bus_test.cpp
+++ tests/bus_test.cpp
@@ -91,4 +91,43 @@ BOOST_AUTO_TEST_CASE(overflow_test)
     BOOST_REQUIRE(!pmessage);
 }
 
+BOOST_AUTO_TEST_CASE(overflow_test1)
+{
+    pmessage_type pmessage;
+    pbus_type pbus1 = bus::make<single_output_bus_type>("test");
+    pbus_type pbus2 = bus::make<single_input_bus_type>("test");
+    BOOST_REQUIRE(pbus1);
+    BOOST_REQUIRE(pbus2);
+    bus::specification_type spec;
+    spec.id = 1;
+    spec.keep_alive_timeout = 0;
+    spec.min_capacity = 8 * 512;
+    spec.max_capacity = 64 * 512;
+    spec.capacity_factor = 50;
+    BOOST_REQUIRE(pbus1->create(spec));
+    BOOST_REQUIRE(pbus2->open());
+    BOOST_REQUIRE(!pbus1->get());
+    BOOST_REQUIRE(!pbus1->pop());
+    BOOST_REQUIRE(!pbus2->get());
+    buffer_t buffer = make_buffer(512);
+    for (size_t i = 0; i < 48; ++i)
+    {
+        BOOST_TEST_MESSAGE("push " << i << "message");
+        BOOST_REQUIRE(pbus1->push(0, &buffer[0], buffer.size()));
+    }
+    for (size_t i = 0; i < 48; ++i)
+    {
+        BOOST_TEST_MESSAGE("get " << i << "message");
+        pmessage = pbus2->get();
+        BOOST_REQUIRE(pmessage);
+        BOOST_REQUIRE_EQUAL(pmessage->data_size(), buffer.size());
+        buffer_t data(pmessage->data_size());
+        pmessage->unpack(&data[0]);
+        BOOST_REQUIRE_EQUAL_COLLECTIONS(buffer.begin(), buffer.end(),
+            data.begin(), data.end());
+        BOOST_REQUIRE(pbus2->pop());
+    }
+    pmessage = pbus2->get();
+    BOOST_REQUIRE(!pmessage);
+}
 
